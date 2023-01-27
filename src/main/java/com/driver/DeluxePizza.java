647174216c7d310041dd158f04911cb610f0b58a
habeeb <habeeb516444@gmail.com>
2023-01-27 11:30:33 +0530

    Solution

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,7 +5,6 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        super(isVeg); // calling parent constructor
         addExtraCheese();
         addExtraToppings();
     }
