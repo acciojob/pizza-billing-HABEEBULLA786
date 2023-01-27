9e1d889113e311a149931655ce4d5a18010a0b1c
habeeb <habeeb516444@gmail.com>
2023-01-27 11:16:34 +0530

    Pizz Generation

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        super(isVeg); // calling parent constructor
+        addExtraCheese();
+        addExtraToppings();
     }
 }
